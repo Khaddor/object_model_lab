df23d906dddd631c54a07a456ed5e71c61a88611
El Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2023-03-14 13:10:28 +0100

    Class Data, TimeSeries et Datum

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 export const version = () => '1.0.0';
 
 /* TODO : Créer le modèle objet ici */
+
+
+class Data {
+    constructor(){}
+}
+
+class TimeSeries extends Data {
+    constructor(){
+        super()
+        this.values = []
+        this.labels = []
+    }
+
+    setValue(value){this.values.push(value)}
+    setLabel(label){this.labels.push(label)}
+
+    getValues(){return this.values;}
+    getLabels(){return this.labels;}
+}
+
+class Datum extends Data {
+    constructor(value){
+        super();
+        this.value = value;
+    }
+
+    setValue(value){this.value = value;}
+    getValue(){return this.value;}
+
+}
